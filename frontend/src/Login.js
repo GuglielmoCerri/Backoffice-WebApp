def69dc44bfdac114d8c3460c23c39b5bee140eb
GuglielmoCerri <cerriguglielmo@gmail.com>
2024-05-25 18:17:51 +0200

    added Login form

---
+++ frontend/src/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import './Login.css'; 
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <div className="login-box">
+      <h2>Login Form</h2>
+      <form onSubmit={handleLogin}>
+        <div className="textbox">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter your username"
+            required
+          />
+        </div>
+        <div className="textbox">
+          <label htmlFor="password">Password:</label>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+            required
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="show-password-button"
+          >
+            <FontAwesomeIcon 
+                icon={showPassword ? faEyeSlash : faEye} 
+                className="password-icon"
+            />
+          </button>
+        </div>
+        <button type="submit" className="btn">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
