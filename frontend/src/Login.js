571340074a83d2585bcccc40788d79d726cb0a99
GuglielmoCerri <cerriguglielmo@gmail.com>
2024-06-03 21:32:23 +0200

    added logic to Login and Registration

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import './Login.css'; 
@@ -8,16 +9,34 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false);
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    console.log(`Username: ${username}, Password: ${password}`);
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/login', {username, password});
+      localStorage.setItem('token', response.data.access_token);
+      alert('Login successful!');
+    } catch (error) {
+      alert('Login failed');
+    }
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/register', {username, password});
+      alert('Registration successful!');
+      setIsRegistering(false);
+    } catch (error) {
+      alert('Registration failed: ' + (error.response ? error.response.data.message : error.message));
+    }
   };
 
   return (
     <div className="login-box">
-      <h2>Login Form</h2>
-      <form onSubmit={handleLogin}>
+      <h2>{isRegistering ? 'Register' : 'Login'} Form</h2>
+      <form onSubmit={isRegistering ? handleRegister : handleLogin}>
         <div className="textbox">
           <label htmlFor="username">Username:</label>
           <input
@@ -45,8 +64,8 @@ const Login = () => {
             className="show-password-button"
           >
             <FontAwesomeIcon 
-                icon={showPassword ? faEyeSlash : faEye} 
-                className="password-icon"
+              icon={showPassword ? faEyeSlash : faEye} 
+              className="password-icon"
             />
           </button>
         </div>
@@ -57,10 +76,13 @@ const Login = () => {
             checked={rememberMe}
             onChange={() => setRememberMe(!rememberMe)}
           />
-          <label htmlFor="remember">Remember</label>
+          <label htmlFor="rememberMe">Remember</label>
         </div>
-        <button type="submit" className="btn">Login</button>
+        <button type="submit" className="btn">{isRegistering ? 'Register' : 'Login'}</button>
       </form>
+      <button onClick={() => setIsRegistering(!isRegistering)} className="btn-switch">
+        {isRegistering ? 'Switch to Login' : 'Switch to Register'}
+      </button>
     </div>
   );
 };
