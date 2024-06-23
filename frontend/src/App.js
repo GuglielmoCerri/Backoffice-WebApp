be44661a679852fc0a3bcbd1f652fa334407aabe
GuglielmoCerri <cerriguglielmo@gmail.com>
2024-06-23 18:37:48 +0200

    Feat: Added functionalities to Remember me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Login from './Login';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+      navigate('/analytics');
+    }
+  }, [navigate]);
+
   return (
     <div className="App">
-      <Login />
+      {!isAuthenticated && <Login />}
     </div>
   );
 }
